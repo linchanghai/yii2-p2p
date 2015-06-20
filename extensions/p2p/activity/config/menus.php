998f5dd35390b02e4acd4ef0177a78866ede4146
changhai.lin <1079140464@qq.com>
2015-06-20 14:48:53 +0800

    no message

--- extensions/p2p/activity/config/menus.php
+++ extensions/p2p/activity/config/menus.php
@@ -2,45 +2,58 @@
 
 return [
     'activity' => [
-        'label' => Yii::t('app', 'Activity'),
+        'label' => Yii::t('p2p_activity', 'Activity'),
         'sort' => 2000,
-        'url' => ['p2p_activity'],
+        'url' => ['/p2p_activity/project/index'],
         'items' => [
-            'project' => [
-                'label' => Yii::t('p2p_activity', 'Project'),
-                'sort' => 100,
-                'url' => ['/p2p_activity/project/index'],
-            ],
-            'project_details' => [
-                'label' => Yii::t('p2p_activity', 'Project Details'),
-                'sort' => 200,
-                'url' => ['/p2p_activity/project-details/index'],
-            ],
-            'project_invest_point_record' => [
-                'label' => Yii::t('p2p_activity', 'Project Invest Point Record'),
+            'activity' => [
+                'label' => Yii::t('p2p_activity', 'Activity'),
                 'sort' => 300,
-                'url' => ['/p2p_activity/project-invest-point-record/index'],
-            ],
-            'project_invest_record' => [
-                'label' => Yii::t('p2p_activity', 'Project Invest Record'),
-                'sort' => 400,
-                'url' => ['/p2p_activity/project-invest-record/index'],
-            ],
-            'project_legal_opinion' => [
-                'label' => Yii::t('p2p_activity', 'Project Legal Opinion'),
-                'sort' => 500,
-                'url' => ['/p2p_activity/project-legal-opinion/index'],
-            ],
-            'project_material' => [
-                'label' => Yii::t('p2p_activity', 'Project Material'),
-                'sort' => 600,
-                'url' => ['/p2p_activity/project-material/index'],
-            ],
-            'project_repayment_record' => [
-                'label' => Yii::t('p2p_activity', 'Project Repayment Record'),
-                'sort' => 700,
-                'url' => ['/p2p_activity/project-repayment-record/index'],
-            ],
+                'items' => [
+                    'project' => [
+                        'label' => Yii::t('p2p_activity', 'Project'),
+                        'sort' => 100,
+                        'url' => ['/p2p_activity/project/index'],
+                        'activeUrls' => [['/p2p_activity/project/create'], ['/p2p_activity/project/update']],
+                    ],
+                    'project_details' => [
+                        'label' => Yii::t('p2p_activity', 'Project Details'),
+                        'sort' => 200,
+                        'url' => ['/p2p_activity/project-details/index'],
+                        'activeUrls' => [['/p2p_activity/project-details/create'], ['/p2p_activity/project-details/update']],
+                    ],
+                    'project_invest_point_record' => [
+                        'label' => Yii::t('p2p_activity', 'Project Invest Point Record'),
+                        'sort' => 300,
+                        'url' => ['/p2p_activity/project-invest-point-record/index'],
+                        'activeUrls' => [['/p2p_activity/project-invest-point-record/create'], ['/p2p_activity/project-invest-point-record/update']],
+                    ],
+                    'project_invest_record' => [
+                        'label' => Yii::t('p2p_activity', 'Project Invest Record'),
+                        'sort' => 400,
+                        'url' => ['/p2p_activity/project-invest-record/index'],
+                        'activeUrls' => [['/p2p_activity/project-invest-record/create'], ['/p2p_activity/project-invest-record/update']],
+                    ],
+                    'project_legal_opinion' => [
+                        'label' => Yii::t('p2p_activity', 'Project Legal Opinion'),
+                        'sort' => 500,
+                        'url' => ['/p2p_activity/project-legal-opinion/index'],
+                        'activeUrls' => [['/p2p_activity/project-legal-opinion/create'], ['/p2p_activity/project-legal-opinion/update']],
+                    ],
+                    'project_material' => [
+                        'label' => Yii::t('p2p_activity', 'Project Material'),
+                        'sort' => 600,
+                        'url' => ['/p2p_activity/project-material/index'],
+                        'activeUrls' => [['/p2p_activity/project-material/create'], ['/p2p_activity/project-material/update']],
+                    ],
+                    'project_repayment_record' => [
+                        'label' => Yii::t('p2p_activity', 'Project Repayment Record'),
+                        'sort' => 700,
+                        'url' => ['/p2p_activity/project-repayment-record/index'],
+                        'activeUrls' => [['/p2p_activity/project-repayment-record/create'], ['/p2p_activity/project-repayment-record/update']],
+                    ],
+                ]
+            ]
         ]
     ],
 ];
