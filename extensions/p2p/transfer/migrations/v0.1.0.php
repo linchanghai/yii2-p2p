8668eb42531cd50428e100b3f17de22f693679cd
lujie.zhou <gao_lujie@live.cn>
2015-06-23 11:09:06 +0800

    add transfer model & migration

---
+++ extensions/p2p/transfer/migrations/v0.1.0.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @link http://www.yincart.com/
+ * @copyright Copyright (c) 2014 Yincart
+ * @license http://www.yincart.com/license/
+ */
+
+namespace p2p\transfer\migrations;
+
+use kiwi\db\Migration;
+use yii\db\Schema;
+
+class v0_1_0 extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%project_invest_transfer_apply}}', [
+            'project_invest_transfer_apple' => Schema::TYPE_PK,
+            'project_invest_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'project_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'member_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'min_money' => Schema::TYPE_INTEGER . '(11) NOT NULL default \'0\'',
+            'total_invest_money' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'discount_rate' => Schema::TYPE_DECIMAL . '(3,2) NOT NULL default \'0.00\'',
+            'status' => Schema::TYPE_SMALLINT . '(1) NOT NULL default \'0\'',
+            'verify_user' => Schema::TYPE_STRING . '(80)',
+            'verify_date' => Schema::TYPE_INTEGER . '(11)',
+            'counter_fee' => Schema::TYPE_DECIMAL . '(8,2) NOT NULL default \'0.00\'',
+            'create_time' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'update_time' => Schema::TYPE_INTEGER . '(11)',
+            'is_delete' => Schema::TYPE_SMALLINT . '(1) NOT NULL default \'0\'',
+        ]);
+
+        $this->createTable('{{%project_transfer_discount_record}}', [
+            'project_transfer_discount_record_id' => Schema::TYPE_PK,
+            'project_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'project_invest_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'member_id' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'rate' => Schema::TYPE_DECIMAL . '(3,2) NOT NULL default \'0.00\'',
+            'discount_money' => Schema::TYPE_DECIMAL . '(3,2) NOT NULL default \'0.00\'',
+            'create_time' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+            'is_delete' => Schema::TYPE_SMALLINT . '(1) NOT NULL default \'0\'',
+        ]);
+    }
+
+    public function safeDown()
+    {
+
+    }
+}
