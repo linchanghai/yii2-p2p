0ad42150fb17dde495b505725765d1cc78d58b49
gezhi <computerwolf@sina.com>
2015-06-28 12:57:30 +0800

    investList

---
+++ frontend/web/investList.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Examples</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="css/style.min.css" rel="stylesheet" />
+    <link href="css/invest.min.css" rel="stylesheet" />
+</head>
+<body>
+<?php include "header.php" ;?>
+<div class="investTitle backGrey">
+    <ul class="container">
+        <li class="active">
+            <a href="#">项目列表</a>
+        </li>
+        <li>
+            <a href="#">项目转让列表</a>
+        </li>
+    </ul>
+</div>
+<div class="investFilter">
+    <div class="container">
+        <div class="clearFix mt10 filterLine">
+            <label>项目期限:</label>
+            <a class="active" href="#">全部</a>
+            <a href="#">一个月以内</a>
+            <a href="#">1-3个月</a>
+            <a href="#">3-6个月</a>
+            <a href="#">6个月以上</a>
+        </div>
+        <div class="clearFix mt10 filterLine">
+            <label>项目收益:</label>
+            <a class="active" href="#">全部</a>
+            <a href="#">9%以下</a>
+            <a href="#">9%-13%</a>
+            <a href="#">13%以上</a>
+        </div>
+    </div>
+</div>
+<div class="mt20 investList">
+    <ul class="container">
+        <li>
+            <div class="proTitle col333 textCenter fs16">
+                汽车宝NO.000020期
+            </div>
+            <div class="progress mt20">
+                <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+            </div>
+            <p class="col666">100000 /<span class="col999">1000000</span></p>
+            <p class="fs12 mt10 investListBrief">本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品</p>
+            <div class="clearFix mt20">
+                <div class="fl investListDetail">
+                    <p class="textCenter themeColor">11%</p>
+                    <p class="textCenter mt10">年利率</p>
+                </div>
+                <div class="fr investListDetail">
+                    <p class="textCenter">100天</p>
+                    <p class="textCenter mt10">期限</p>
+                </div>
+            </div>
+            <a class="checkItem" href="#">
+                立即投资
+            </a>
+        </li>
+        <li>
+            <div class="proTitle col333 textCenter fs16">
+                汽车宝NO.000020期
+            </div>
+            <div class="progress mt20">
+                <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+            </div>
+            <p class="col666">100000 /<span class="col999">1000000</span></p>
+            <p class="fs12 mt10 investListBrief">本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品</p>
+            <div class="clearFix mt20">
+                <div class="fl investListDetail">
+                    <p class="textCenter themeColor">11%</p>
+                    <p class="textCenter mt10">年利率</p>
+                </div>
+                <div class="fr investListDetail">
+                    <p class="textCenter">100天</p>
+                    <p class="textCenter mt10">期限</p>
+                </div>
+            </div>
+            <a class="checkItem" href="#">
+                立即投资
+            </a>
+        </li>
+        <li>
+            <div class="proTitle col333 textCenter fs16">
+                汽车宝NO.000020期
+            </div>
+            <div class="progress mt20">
+                <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+            </div>
+            <p class="col666">100000 /<span class="col999">1000000</span></p>
+            <p class="fs12 mt10 investListBrief">本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品</p>
+            <div class="clearFix mt20">
+                <div class="fl investListDetail">
+                    <p class="textCenter themeColor">11%</p>
+                    <p class="textCenter mt10">年利率</p>
+                </div>
+                <div class="fr investListDetail">
+                    <p class="textCenter">100天</p>
+                    <p class="textCenter mt10">期限</p>
+                </div>
+            </div>
+            <a class="checkItem" href="#">
+                立即投资
+            </a>
+        </li>
+        <li>
+            <div class="proTitle col333 textCenter fs16">
+                汽车宝NO.000020期
+            </div>
+            <div class="progress mt20">
+                <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+            </div>
+            <p class="col666">100000 /<span class="col999">1000000</span></p>
+            <p class="fs12 mt10 investListBrief">本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品</p>
+            <div class="clearFix mt20">
+                <div class="fl investListDetail">
+                    <p class="textCenter themeColor">11%</p>
+                    <p class="textCenter mt10">年利率</p>
+                </div>
+                <div class="fr investListDetail">
+                    <p class="textCenter">100天</p>
+                    <p class="textCenter mt10">期限</p>
+                </div>
+            </div>
+            <a class="checkItem" href="#">
+                立即投资
+            </a>
+        </li>
+        <li>
+            <div class="proTitle col333 textCenter fs16">
+                汽车宝NO.000020期
+            </div>
+            <div class="progress mt20">
+                <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+            </div>
+            <p class="col666">100000 /<span class="col999">1000000</span></p>
+            <p class="fs12 mt10 investListBrief">本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品本项目为个人贷款产品</p>
+            <div class="clearFix mt20">
+                <div class="fl investListDetail">
+                    <p class="textCenter themeColor">11%</p>
+                    <p class="textCenter mt10">年利率</p>
+                </div>
+                <div class="fr investListDetail">
+                    <p class="textCenter">100天</p>
+                    <p class="textCenter mt10">期限</p>
+                </div>
+            </div>
+            <a class="checkItem" href="#">
+                立即投资
+            </a>
+        </li>
+    </ul>
+</div>
+<?php include "footer.php" ;?>
+<script type="text/javascript" src="js/invest.js"></script>
+</body>
+</html>
---
+++ frontend/web/js/invest.js
@@ -0,0 +1,6 @@
+require(["jquery","common"],function($){
+    $(function(){
+
+
+    });
+});
