3854c31c7f3c39c48bba51076bbf79505241bce0
gezhi <computerwolf@sina.com>
2015-06-30 20:29:47 +0800

    sss

---
+++ frontend/web/invest.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Examples</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href="css/style.min.css" rel="stylesheet" />
+    <link href="css/invest.min.css" rel="stylesheet" />
+</head>
+<body>
+<?php include "header.php" ;?>
+<div class="container mt20 investTitleTop">
+    <div class="fl investTitleLeft">短期理财</div>
+    <div class="fr investTitleRight">
+         年化收益率高达12%
+        <a class="fr" href="#">更多 ></a>
+    </div>
+</div>
+<div class="container investWrap">
+    <div class="fl progressWrap">
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped" style="width: 10%;"></div>
+        </div>
+        <p class="mt20 fs30 textCenter themeColor">10%</p>
+    </div>
+    <div class="fl progressWrapDetail">
+        <p class="mb20">
+            <label>产品期号: </label><span class="col333">Z00000089期</span>
+        </p>
+        <p class="mb20">
+            <label>项目金额: </label><span class="col333">2000000元</span>
+        </p>
+        <p class="mb20">
+            <label>剩余金额: </label><span class="col333">1800000元</span>
+        </p>
+        <p class="mb20">
+            <label>收款方式: </label><span class="col333">按月付息，到期还本</span>
+        </p>
+        <p class="mb20">
+            <label>还款日期: </label><span class="col333">2015-09-25</span>
+        </p>
+    </div>
+    <div class="fl progressWrapDetail progressWrapDetail2">
+        <p class="mb20">
+            <label>理财期限: </label><span class="col333">30天</span>
+        </p>
+        <p class="mb20">
+            <label>预期年化收益率: </label><span class="col333">8%</span>
+        </p>
+        <p class="mb20">
+            <label>奖励说明: </label><span class="col333">送积分，送会员成长值</span>
+        </p>
+    </div>
+    <div class="fl investArea">
+
+    </div>
+</div>
+
+<?php include "footer.php" ;?>
+<script type="text/javascript" src="js/invest.js"></script>
+</body>
+</html>
