9a6ab1b9e36289760d40a1eea90cb6705a148f89
changhai.lin <1079140464@qq.com>
2015-07-03 10:30:40 +0800

    no message

--- extensions/p2p/project/views/frontend/project-invest/prepare.php
+++ extensions/p2p/project/views/frontend/project-invest/prepare.php
@@ -11,11 +11,12 @@ $projectRepayments = $InvestPrepareForm->projectRepayments;
 ?>
 <p>投资金额：<?= $InvestPrepareForm->invest_money; ?></p>
 <p>年化收益：<?= $InvestPrepareForm->rate; ?>%</p>
-<p>可获得收益：<?= $InvestPrepareForm->interest_money; ?></p>
+<p>可获得收益：<?= floor($InvestPrepareForm->interest_money * 100) / 100; ?></p>
+<p>实际投资金额：<?= $InvestPrepareForm->actual_invest_money; ?></p>
 <?php
 /** @var \p2p\project\models\ProjectRepayment $projectRepayment */
 foreach($projectRepayments as $projectRepayment) {
     ?>
     <p>付息时间：<?= date('Y-m-d', $projectRepayment->repayment_date); ?></p>
-    <p>支付利息：<?= $projectRepayment->interest_money; ?></p>
+    <p>支付利息：<?= floor($projectRepayment->interest_money * 100) / 100; ?></p>
 <?php } ?>
