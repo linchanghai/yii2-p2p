ac247dc795850c59e9ab6b1e4a32d95173cde1f7
changhai.lin <1079140464@qq.com>
2015-07-06 09:52:28 +0800

    no message

--- extensions/p2p/project/views/frontend/project/details.php
+++ extensions/p2p/project/views/frontend/project/details.php
@@ -79,7 +79,114 @@ $this->registerJsFile(Yii::$app->urlManager->baseUrl . '/js/invest.js', ['depend
             <span class="col333">送积分，送会员成长值</span>
         </p>
     </div>
-    <div class="fl investArea">
-
+    <div class="fr investArea">
+        <p class="mt10">
+            可投金额: <span class="fs16" id="mostMoney"><?= $project->invest_total_money - $project->invested_money; ?></span> 元
+        </p>
+        <p class="mt20">
+            起息方式: T(成交日)+1
+        </p>
+        <p class="mt20"><span class="themeColor" id="leastMoney"><?= $project->min_money; ?></span> 元起投</p>
+        <p class="toInvestArea">
+            <span class="btn themeBtn toInvest">立即投资</span><span class="btn toInvest toCalc">计算</span>
+        </p>
+    </div>
+</div>
+<div class="container mt20 backGrey investRule">
+    <ul class="clearFix investRuleTap">
+        <li class="active">项目详情</li>
+        <li>项目图片资料</li>
+        <li>法律意见书</li>
+        <li>投资记录</li>
+    </ul>
+    <div class="investRuleDetail">
+        <div class="investSingleRule">
+            <table class="table tableNoBorder investSingleRule1">
+                <tbody>
+                <tr>
+                    <td class="textRight">保障方式</td>
+                    <td>100%本息保障计划</td>
+                </tr>
+                <tr>
+                    <td class="textRight">可加入日期</td>
+                    <td>2015-07-07</td>
+                </tr>
+                <tr>
+                    <td class="textRight">预期年化收益率</td>
+                    <td class="secondColor">12%</td>
+                </tr>
+                <tr>
+                    <td class="textRight">加入条件</td>
+                    <td class="secondColor">	<?= $project->min_money; ?>元起，以<?= $project->min_money; ?>元的倍数递增</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="investSingleRule hide">2</div>
+        <div class="investSingleRule hide">3</div>
+        <div class="investSingleRule hide">4</div>
+    </div>
+</div>
+<div class="modal fade" id="investModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">填写投资金额</h4>
+            </div>
+            <div class="modal-body">
+                <form class="clearFix prepInvest" method="post" action="">
+                    <div class="fl investModalArea">
+                        <div class="clearFix">
+                            <label class="fl">投资金额:</label>
+                            <div class="fl ml10 moneyArea">
+                                <span class="fl operate minus">-</span>
+                                <input type="text" name="" value="1000" class="fl investMoney" />
+                                <span class="fl operate plus">+</span>
+                            </div>
+                        </div>
+                        <p class="mt20">账户余额: <span id="myMoney">1000</span>元 </p>
+                    </div>
+                    <div class="fr">
+                        <button class="btn largeBtn investNow" type="submit">确认投资</button>
+                    </div>
+                </form>
+                <div class="mt20 investSingleMoney">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th>年化收益率</th>
+                            <th>可获得收益: 200元</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>付息时间: 2015-07-07</td>
+                            <td>支付利息: 20元</td>
+                        </tr>
+                        <tr>
+                            <td>付息时间: 2015-08-07</td>
+                            <td>支付利息: 20元</td>
+                        </tr>
+                        <tr>
+                            <td>付息时间: 2015-09-07</td>
+                            <td>支付利息: 20元</td>
+                        </tr>
+                        <tr>
+                            <td>付息时间: 2015-09-07</td>
+                            <td>支付利息: 20元</td>
+                        </tr>
+                        <tr>
+                            <td>付息时间: 2015-09-07</td>
+                            <td>支付利息: 20元</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a class="btn regularBtn themeBtn" href="#">充值</a>
+            </div>
+        </div>
     </div>
 </div>
