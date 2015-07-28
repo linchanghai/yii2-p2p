bfe7be5c2c7101f43a04c9781f5d6f7f896ca047
changhai.lin <1079140464@qq.com>
2015-07-28 09:40:49 +0800

    no message

--- frontend/views/layouts/account.php
+++ frontend/views/layouts/account.php
@@ -23,6 +23,9 @@ $this->registerJsFile('/js/account.js', ['depends' => [AppAsset::className()]]);
             'recharge/recharge/recharge-list',
             'withdraw/withdraw/withdraw',
             'withdraw/withdraw/withdraw-list',
+            'package/package/index',
+            'package/package/into',
+            'package/package/out',
             'package/package-record/package-list',
             'package/package-record/into-list',
             'package/package-record/out-list',
@@ -40,6 +43,9 @@ $this->registerJsFile('/js/account.js', ['depends' => [AppAsset::className()]]);
             ]) ? 'active' : '' ?>">
                 <a href="<?= Url::to(['/withdraw/withdraw/withdraw-list']) ?>">提现管理</a></dd>
             <dd class="<?= in_array(Yii::$app->request->pathInfo, [
+                'package/package/index',
+                'package/package/into',
+                'package/package/out',
                 'package/package-record/package-list',
                 'package/package-record/into-list',
                 'package/package-record/out-list',
@@ -48,9 +54,12 @@ $this->registerJsFile('/js/account.js', ['depends' => [AppAsset::className()]]);
             <dd class="<?= (Yii::$app->request->pathInfo == 'member/statistic-change/statistic-list') ? 'active' : '' ?>">
                 <a href="<?= Url::to(['/member/statistic-change/statistic-list']) ?>">资金流水</a></dd>
         </dl>
-        <dl>
+        <dl class="<?= in_array(Yii::$app->request->pathInfo, [
+            'project/project-invest/grid-view',
+        ]) ? 'current' : '' ?>">
             <dt><a href="#"><i class="glyphicon glyphicon-yen fs16"></i>理财管理</a></dt>
-            <dd><a href="<?= Url::to(['/project/project-invest/grid-view']) ?>">投资记录</a></dd>
+            <dd class="<?= in_array(Yii::$app->request->pathInfo, ['project/project-invest/grid-view']) ? 'active' : '' ?>">
+                <a href="<?= Url::to(['/project/project-invest/grid-view']) ?>">投资记录</a></dd>
             <dd><a href="#">债券转让</a></dd>
             <dd><a href="#">资产统计</a></dd>
         </dl>
